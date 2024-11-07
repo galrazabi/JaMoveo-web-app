2cac7fe379e481e1bdc5b6eda4fa3cf4554574c1
gal.razabi <galrazabi@gmail.com>
2024-11-07 16:06:27 +0200

    reg

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,23 @@
+import './Home.css'
+
 export const Home = () => {
     return (
         <h1>Welcome to JaMoveo</h1>
+        // <body>
+        //     <main>
+        //         <section class="intro">
+        //             <h2>Welcome to JaMoveo</h2>
+        //             <p>Your ultimate platform for streamlined, real-time rehearsals. </p>
+        //             <div class="buttons">
+        //                 <a href="/signup" class="btn-signup">Sign Up</a>
+        //                 <a href="/login" class="btn-login">Log In</a>
+        //             </div>
+        //         </section>
+        //     </main>
+
+        //     <footer>
+        //         <p>© 2024 JaMoveo. All rights reserved.</p>
+        //     </footer>
+        // </body>
     )
 }
